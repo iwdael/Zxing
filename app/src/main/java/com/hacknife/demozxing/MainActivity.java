12dee9628d8be7aab6c371dc2530074a4fb79759
Hacknife <4884280@qq.com>
2018-10-28 10:39:30 +0800

    demo

--- app/src/main/java/com/hacknife/demozxing/MainActivity.java
+++ app/src/main/java/com/hacknife/demozxing/MainActivity.java
@@ -1,13 +1,30 @@
 package com.hacknife.demozxing;
 
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.SurfaceView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.zxing.Result;
+import com.hacknife.zxing.BaseCaptureActivity;
+
+public class MainActivity extends BaseCaptureActivity {
+    SurfaceView surfaceView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        surfaceView = findViewById(R.id.surface);
+    }
+
+    @Override
+    public SurfaceView getSurfaceView() {
+        return surfaceView;
+    }
+
+    @Override
+    public void dealDecode(Result rawResult, Bitmap barcode, float scaleFactor) {
+
     }
 }
