123fa0a72288f357633afa5ab8076a07fc390cdb
hacknife <hacknife@outlook.com>
2019-03-27 16:32:21 +0800

    ReadMe

--- library/src/main/java/com/hacknife/zxing/util/QRHelper.java
+++ library/src/main/java/com/hacknife/zxing/util/QRHelper.java
@@ -2,14 +2,25 @@ package com.hacknife.zxing.util;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.support.annotation.ColorInt;
+import android.text.TextUtils;
 
+import com.google.zxing.BarcodeFormat;
 import com.google.zxing.BinaryBitmap;
+import com.google.zxing.EncodeHintType;
 import com.google.zxing.LuminanceSource;
 import com.google.zxing.MultiFormatReader;
 import com.google.zxing.NotFoundException;
 import com.google.zxing.PlanarYUVLuminanceSource;
 import com.google.zxing.Result;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
 import com.google.zxing.common.HybridBinarizer;
+import com.google.zxing.qrcode.QRCodeWriter;
+
+import java.util.Hashtable;
 
 /**
  * author  : hacknife
@@ -58,8 +69,150 @@ public class QRHelper {
         } catch (NotFoundException e) {
             e.printStackTrace();
         }
-
         return result;
     }
 
+    public static Result readQrImage(Bitmap scanBitmap) {
+        
+        LuminanceSource luminanceSource = new PlanarYUVLuminanceSource(
+                rgb2YUV(scanBitmap), scanBitmap.getWidth(),
+                scanBitmap.getHeight(), 0, 0, scanBitmap.getWidth(),
+                scanBitmap.getHeight(), false);
+        BinaryBitmap binaryBitmap = new BinaryBitmap(new HybridBinarizer(luminanceSource));//把可视图片转为二进制图片
+        Result result = null;//解析图片中的code
+        try {
+            result = new MultiFormatReader().decode(binaryBitmap);
+        } catch (NotFoundException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+
+    /**
+     * 创建二维码位图
+     */
+    
+    public static Bitmap createQRCodeBitmap( String content, int size, int padding) {
+        return createQRCodeBitmap(content, size, "UTF-8", "H", padding + "", Color.BLACK, Color.WHITE, null, null, 0F);
+    }
+
+    /**
+     * 创建二维码位图 (带Logo小图片)
+     */
+    
+    public static Bitmap createQRCodeBitmap(String content, int size, int padding,  Bitmap logoBitmap, float logoPercent) {
+        return createQRCodeBitmap(content, size, "UTF-8", "H", "" + padding, Color.BLACK, Color.WHITE, null, logoBitmap, logoPercent);
+    }
+
+
+    /**
+     * 创建二维码位图 (支持自定义配置和自定义样式)
+     */
+    
+    public static Bitmap createQRCodeBitmap(String content, int size,
+                                            String character_set, String error_correction, String margin,
+                                            @ColorInt int color_black, @ColorInt int color_white, Bitmap targetBitmap,
+                                            Bitmap logoBitmap, float logoPercent) {
+
+        /** 1.参数合法性判断 */
+        if (TextUtils.isEmpty(content)) { // 字符串内容判空
+            return null;
+        }
+
+        if (size <= 0) { // 宽&高都需要>0
+            return null;
+        }
+
+        try {
+            /** 2.设置二维码相关配置,生成BitMatrix(位矩阵)对象 */
+            Hashtable<EncodeHintType, String> hints = new Hashtable<>();
+
+            if (!TextUtils.isEmpty(character_set)) {
+                hints.put(EncodeHintType.CHARACTER_SET, character_set); // 字符转码格式设置
+            }
+
+            if (!TextUtils.isEmpty(error_correction)) {
+                hints.put(EncodeHintType.ERROR_CORRECTION, error_correction); // 容错级别设置
+            }
+
+            if (!TextUtils.isEmpty(margin)) {
+                hints.put(EncodeHintType.MARGIN, margin); // 空白边距设置
+            }
+            BitMatrix bitMatrix = new QRCodeWriter().encode(content, BarcodeFormat.QR_CODE, size, size, hints);
+
+            /** 3.根据BitMatrix(位矩阵)对象为数组元素赋颜色值 */
+            if (targetBitmap != null) {
+                targetBitmap = Bitmap.createScaledBitmap(targetBitmap, size, size, false);
+            }
+            int[] pixels = new int[size * size];
+            for (int y = 0; y < size; y++) {
+                for (int x = 0; x < size; x++) {
+                    if (bitMatrix.get(x, y)) { // 黑色色块像素设置
+                        if (targetBitmap != null) {
+                            pixels[y * size + x] = targetBitmap.getPixel(x, y);
+                        } else {
+                            pixels[y * size + x] = color_black;
+                        }
+                    } else { // 白色色块像素设置
+                        pixels[y * size + x] = color_white;
+                    }
+                }
+            }
+
+            /** 4.创建Bitmap对象,根据像素数组设置Bitmap每个像素点的颜色值,之后返回Bitmap对象 */
+            Bitmap bitmap = Bitmap.createBitmap(size, size, Bitmap.Config.ARGB_8888);
+            bitmap.setPixels(pixels, 0, size, 0, 0, size, size);
+
+            /** 5.为二维码添加logo小图标 */
+            if (logoBitmap != null) {
+                return addLogo(bitmap, logoBitmap, logoPercent);
+            }
+
+            return bitmap;
+        } catch (WriterException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    /**
+     * 向一张图片中间添加logo小图片(图片合成)
+     */
+    
+    private static Bitmap addLogo( Bitmap srcBitmap,  Bitmap logoBitmap, float logoPercent) {
+
+        /** 1. 参数合法性判断 */
+        if (srcBitmap == null) {
+            return null;
+        }
+
+        if (logoBitmap == null) {
+            return srcBitmap;
+        }
+
+        if (logoPercent < 0F || logoPercent > 1F) {
+            logoPercent = 0.2F;
+        }
+
+        /** 2. 获取原图片和Logo图片各自的宽、高值 */
+        int srcWidth = srcBitmap.getWidth();
+        int srcHeight = srcBitmap.getHeight();
+        int logoWidth = logoBitmap.getWidth();
+        int logoHeight = logoBitmap.getHeight();
+
+        /** 3. 计算画布缩放的宽高比 */
+        float scaleWidth = srcWidth * logoPercent / logoWidth;
+        float scaleHeight = srcHeight * logoPercent / logoHeight;
+
+        /** 4. 使用Canvas绘制,合成图片 */
+        Bitmap bitmap = Bitmap.createBitmap(srcWidth, srcHeight, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        canvas.drawBitmap(srcBitmap, 0, 0, null);
+        canvas.scale(scaleWidth, scaleHeight, srcWidth / 2, srcHeight / 2);
+        canvas.drawBitmap(logoBitmap, srcWidth / 2 - logoWidth / 2, srcHeight / 2 - logoHeight / 2, null);
+
+        return bitmap;
+    }
 }
